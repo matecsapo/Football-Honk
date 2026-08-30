9aedafe3c505c7d53476b0c886f7c4b0e07423ca
Máté Csapo <matecsapo@Mates-MacBook-Pro.local>
2026-08-30 10:31:06 -0400

    Football-Honk v1.7.0

--- source/honk/config.py
+++ source/honk/config.py
@@ -8,7 +8,7 @@ current_season = Season(2026)
 modelled_leagues = [
         (League("ENG-Premier League"), True),
         (League("ESP-La Liga"), True),
-        (League("GER-Bundesliga"), True),
+        (League("GER-Bundesliga"), False),
         (League("ITA-Serie A"), True),
         (League("FRA-Ligue 1"), True)
     ]
